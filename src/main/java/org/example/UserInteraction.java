82ded9f2c38f0e53b4a314a95bbc89ac21282f74
Kavandeep <kavan1@live.co.uk>
2022-08-11 14:30:03 +0100

    Scanner for user to input chars

--- src/main/java/org/example/UserInteraction.java
+++ src/main/java/org/example/UserInteraction.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class UserInteraction {
+    public String getInput() {
+
+        System.out.println("Guess a letter: ");
+        Scanner scanner = new Scanner( System.in );
+        return scanner.nextLine();
+    }
 }
