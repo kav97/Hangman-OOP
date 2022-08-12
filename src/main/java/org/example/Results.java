ff0918293d0b3fc5491aef3b7fb0af0ed346fc9d
Kavandeep <kavan1@live.co.uk>
2022-08-12 10:07:32 +0100

    7 lives instead of 5

--- src/main/java/org/example/Results.java
+++ src/main/java/org/example/Results.java
@@ -2,19 +2,23 @@ package org.example;
 
 public class Results {
 
-    public static void displayLives (int numberOfLivesRemaining) {
-        if(numberOfLivesRemaining == 5) {
-            System.out.println("You have 5 lives remaining");
-        } else if (numberOfLivesRemaining == 4) {
-            System.out.println("You have 4 lives remaining...");
-        } else if (numberOfLivesRemaining == 3) {
-            System.out.println("You have 3 lives remaining...");
-        } else if (numberOfLivesRemaining == 2) {
-            System.out.println("You have 2 lives remaining...");
-        } else if (numberOfLivesRemaining == 1) {
-            System.out.println("You have 1 lives remaining...");
-        } else if (numberOfLivesRemaining == 0) {
-            System.out.println("You have 0 lives remaining... game over.");
+    public static void displayLives ( int numberOfLivesRemaining ) {
+        if ( numberOfLivesRemaining == 7 ) {
+            System.out.println( "You have 7 lives remaining" );
+        } else if ( numberOfLivesRemaining == 6 ) {
+            System.out.println( "You have 6 lives remaining..." );
+        } else if ( numberOfLivesRemaining == 5 ) {
+            System.out.println( "You have 5 lives remaining..." );
+        } else if ( numberOfLivesRemaining == 4 ) {
+            System.out.println( "You have 4 lives remaining..." );
+        } else if ( numberOfLivesRemaining == 3 ) {
+            System.out.println( "You have 3 lives remaining..." );
+        } else if ( numberOfLivesRemaining == 2 ) {
+            System.out.println( "You have 2 lives remaining..." );
+        } else if ( numberOfLivesRemaining == 1 ) {
+            System.out.println( "You have 1 lives remaining..." );
+        } else if ( numberOfLivesRemaining == 0 ) {
+            System.out.println( "You have 0 lives remaining... game over." );
         }
     }
 }
