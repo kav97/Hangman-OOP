91530ad72ef47192fdddb2f97afda194cd9a6d5d
Kavandeep <kavan1@live.co.uk>
2022-08-12 11:46:07 +0100

    Added more animals to Dictionary.java

--- src/main/java/org/example/Dictionary.java
+++ src/main/java/org/example/Dictionary.java
@@ -16,6 +16,7 @@ public class Dictionary {
         animals.add("lion");
         animals.add("llama");
         animals.add("crocodile");
+        animals.add("alligator");
         animals.add("giraffe");
         animals.add("monkey");
         animals.add("zebra");
@@ -30,13 +31,32 @@ public class Dictionary {
         animals.add("cow");
         animals.add("buffalo");
         animals.add("shark");
+        animals.add("sheep");
+        animals.add("goat");
+        animals.add("antelope");
+        animals.add("wolf");
+        animals.add("frog");
+        animals.add("butterfly");
+        animals.add("cheetah");
+        animals.add("elephant");
+        animals.add("turtle");
+        animals.add("kangaroo");
+        animals.add("eagle");
+        animals.add("skunk");
+        animals.add("octopus");
+        animals.add("lobster");
+        animals.add("dolphin");
+        animals.add("penguin");
+        animals.add("koala");
+        animals.add("jellyfish");
+        animals.add("jellyfish");
     }
 
-    public static String generateAnimal() {
+    public static String getAnimal() {
         return animals.get( RANDOM.nextInt( animals.size()) );
     }
 
     public static void main(String[] args) {
-        System.out.println(generateAnimal());
+        System.out.println(getAnimal());
     }
 }
